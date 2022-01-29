d1816dfc5dda59f30b9c2a947b4a1097bb1bbeac
JellytoDev <wjdwhdgus2222@naver.com>
2022-01-29 20:23:01 +0900

    Initial commit

---
+++ src/main/java/spring_practice1/spring_practice1/members/Grade.java
@@ -0,0 +1,4 @@
+package spring_practice1.spring_practice1.members;
+
+public enum Grade {
+}
---
+++ src/test/java/spring_practice1/spring_practice1/SpringPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_practice1.spring_practice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
